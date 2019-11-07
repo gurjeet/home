03cea29d20bb6813dcc08d044d3272ea67af443b
Gurjeet Singh <gurjeet@singh.im>
2019-11-07 12:41:10 -0800

    Added config.nix to control Nix CLIs' behaviour

---
+++ .config/nixpkgs/config.nix
@@ -0,0 +1,6 @@
+{
+  # When you wish to install a Nix package that is not supported on the current
+  # system, remove the DONT_ prefix from the `allowUnsupportedSystem` property.
+  DONT_allowUnsupportedSystem = true;
+}
+
