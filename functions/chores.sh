de83e359f7e1db3d1c900c1ed7fc02b8cb746a6b
Gurjeet Singh <gurjeet@singh.im>
2020-07-24 09:19:46 -0700

    Added function to skip updating Homebrew repos

---
+++ functions/chores.sh
@@ -0,0 +1,19 @@
+
+function morning_chores() {
+    ~/dev/OpenBookmarkGroup.applescript Morning
+    ~/dev/OpenBookmarkGroup.applescript Trading
+    ~/dev/domain_hunt.sh
+}
+
+function afternoon_chores() {
+    ~/dev/OpenBookmarkGroup.applescript Afternoon
+}
+
+function evening_chores() {
+    ~/dev/OpenBookmarkGroup.applescript Evening
+}
+
+function weekly_chores() {
+    ~/dev/OpenBookmarkGroup.applescript Weekly
+}
+
---
+++ functions/homebrew.sh
@@ -0,0 +1,3 @@
+function ,brew() {
+    HOMEBREW_NO_AUTO_UPDATE=1 brew "$@"
+}
