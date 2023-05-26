1724867729cad589d20f74fc6f004ca722c9f45e
Gurjeet Singh <gurjeet@singh.im>
2023-05-26 16:10:13 -0700

    Comment and condense the code in functions/logging.sh
    Use `--ignore-all-space` and `--color-words` Git options to see a
    better/condensed view of this patch.

--- functions/logging.sh
+++ functions/logging.sh
@@ -3,30 +3,18 @@
 # Use a period(.) prefix for these functions, to avoid name collision with
 # other commands.
 
-function .info()
-{
-    echo "INFO: $@" >&1
-}
-
-function .notice()
-{
-    echo "NOTICE: $@" >&1
-}
-
-function .warning()
-{
-    echo "WARNING: $@" >&2
-}
-
-function .error()
-{
-    echo "ERROR: $@" >&2
-}
-
+# All these functions, except .fatal, emit a message level followed by all
+# their arguments. The .fatal function uses the first argument as the exit
+# code, and uses the remaining arguments as do other functions.
+function .info()    { echo    "INFO: $@" >&1; }
+function .notice()  { echo  "NOTICE: $@" >&1; }
+function .warning() { echo "WARNING: $@" >&2; }
+function .error()   { echo   "ERROR: $@" >&2; }
+function .fatalmsg(){ echo   "FATAL: $@" >&2; }
 function .fatal()
 {
-    exitCode="$1"
-    shift
-    echo "FATAL: $@" >&2
-    exit "$exitCode"
+    exitCode="$1";
+    shift;
+    .fatalmsg "$@";
+    exit "$exitCode";
 }
