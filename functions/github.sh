9b19fea1872c8615baa54b8e3198be661143b5df
Gurjeet Singh <gurjeet@singh.im>
2024-07-01 21:17:49 -0700

    Fix github_clone_to_personal_account() to ignore pull-requests

--- functions/github.sh
+++ functions/github.sh
@@ -36,6 +36,9 @@ function github_clone_to_personal_account() {
     git clone --mirror $PUBLIC_REPO_URL $TMP_DIR
     curl --header "Authorization: token $GH_TOKEN" --request POST --data '{"name":"'${REPO_CLONE}'","description":"Clone of https://github.com/'${REPO_OWNER}'/'${REPO_NAME}', as of '$(TZ=UTC date +%Y%m%d:%H%M%S%z)'"}' "https://api.github.com/user/repos"
     git lfs fetch --all
+    # Remove mention of all pull-requests; GitHub throws errors if we try to push these refs.
+    awk '!/ refs\/pull\//' packed-refs > packed-refs.new
+    mv packed-refs.new packed-refs
     git push --mirror "https://$PERSONAL_ACCOUNT:$GH_TOKEN@github.com/$PERSONAL_ACCOUNT/$REPO_CLONE"
     git lfs push --all "https://$PERSONAL_ACCOUNT:$GH_TOKEN@github.com/$PERSONAL_ACCOUNT/$REPO_CLONE"
     popd
