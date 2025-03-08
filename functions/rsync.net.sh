483c2cf59457c5736c6f13787fb96caa90483e54
Gurjeet Singh <gurjeet@singh.im>
2025-03-08 13:40:53 -0800

    Added function rsyncnet_ssh() to run ssh commands

--- functions/rsync.net.sh
+++ functions/rsync.net.sh
@@ -1,11 +1,22 @@
 
-function rsync_net_sshfs_mount()
+function rsyncnet_ssh()
+{
+    ssh fm2343@fm2343.rsync.net "$@"
+}
+
+function rsyncnet_sshfs_mount()
 {
     sudo mkdir -p /Volumes/sshfs
     sudo sshfs fm2343@fm2343.rsync.net: /Volumes/sshfs -oauto_cache,reconnect,local,volname=rsync.net,allow_other,defer_permissions,noappledouble
 }
 
-function rsync_net_sshfs_unmount()
+function rsyncnet_sshfs_unmount()
 {
     sudo umount /Volumes/sshfs
 }
+
+function rsyncnet
+{
+    rsync -a --no-group --no-times "$@"
+}
+
